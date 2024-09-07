c54faf9f5cb675dbd8587798a9e5eb773fbc95e8
ejaj05 <alamejaj288@gmail.com>
2024-09-07 10:32:04 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,11 @@
+let score = "33" // String
+let value = Number(score) // Number
+let num = 1
+let booleanVal = Boolean(num) // true Boolean Value
+let num2 = 0;
+let booleanVal2 = Boolean(num2) // false Boolean value
+
+/*
+"" => false
+"anything" => true
+*/
