e4645d3233210d4b8aa5f15f087ead19fb9a6118
ejaj05 <alamejaj288@gmail.com>
2024-09-07 11:07:46 +0000

    dataType

--- Basics/dataType.js
+++ Basics/dataType.js
@@ -17,3 +17,17 @@ const obj = {
 let myFunc = function(){
     console.log("Hello world!");
 }
+
+// stack (Primitive) Heap (Non-Primitive)
+
+let myName = "Ejaj Alam"
+let anotherName = myName // copy of myName
+anotherName = "Adnan Alam" // only changes in copy 
+
+const userOne = {
+    email:"alamejaj288@gmail.com"
+}
+const userTwo = userOne
+userTwo.email = "adnan@gmail.com"
+
+//both variable are pointing at the same object in Heap
