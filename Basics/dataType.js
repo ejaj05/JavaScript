00c5cecb4385afa601bfd59095b093c55771a012
ejaj05 <alamejaj288@gmail.com>
2024-09-07 10:49:55 +0000

    dataType

---
+++ Basics/dataType.js
@@ -0,0 +1,19 @@
+/* 
+#Primitive
+7 Type: String, Number, Boolean, Undefined, Null, Symbol, BigInt
+
+#Non-Primitive
+Array, Object, Function
+*/
+
+const arr = ["Ejaj", "Adnan", "Sohil"]
+
+const obj = {
+    name:"Ejaj",
+    age:22,
+    city:kolkata
+}
+
+let myFunc = function(){
+    console.log("Hello world!");
+}
