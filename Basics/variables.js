d0cb5030012ea5b2d0ef729c2b089c93d0ecf78b
ejaj05 <alamejaj288@gmail.com>
2024-09-07 02:46:36 +0000

    container config done

---
+++ Basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountName = "Ejaj ALam"
+var accountEmail = "alamejaj288@gmail.com"
+accountCity = "Kolkata"
+let accountState
+
+console.table([accountId,accountEmail,accountCity,accountState])
