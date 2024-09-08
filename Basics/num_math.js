804ac9806f1956039ad7bc1ffdeb1ba6ebb0445a
ejaj05 <alamejaj288@gmail.com>
2024-09-08 05:23:49 +0000

    num&math

---
+++ Basics/num_math.js
@@ -0,0 +1,10 @@
+const score = new Number(4000000)
+console.log(score.toLocaleString('en-IN'))
+
+
+//++++++++++++ MATH ++++++++++++//
+console.log(Math.floor(Math.random()*10)+1)
+const min = 10
+const mx = 25
+const randomNum = Math.floor(Math.random()*(mx-min+1)+min)
+console.log(randomNum)
