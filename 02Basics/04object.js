fe166916387b4d1ca7ef3f94092f7620f511f5b3
ejaj05 <alamejaj288@gmail.com>
2024-09-08 16:55:23 +0000

    Object

--- 02Basics/04object.js
+++ 02Basics/04object.js
@@ -1,3 +1,27 @@
 //singelton object
+//const tinder = new Object()
 
-const tinder = new Object()
+const tinderUser = {}
+tinderUser.email = 'ejaj@google.com'
+tinderUser.id = '123abc'
+tinderUser.pass = '@123abc'
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+const obj1 = {1:'a', 2:'b'}
+const obj2 = {3:'c', 4:'d'}
+
+//const obj3 = Object.assign(obj1,obj2) obj1 === obj3
+const obj3 = Object.assign({},obj1, obj2)
+// console.log(obj1)
+// console.log(obj3)
+
+const arr1 = Object.keys(tinderUser) // keys are strored in Array 
+const arr2 = Object.values(tinderUser) // values are stored in Array
+const arr3 = Object.entries(tinderUser)
+// console.log(arr1)
+// console.log(arr2)
+// console.log(arr3)
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // checks if keys are present in object or not 
+
