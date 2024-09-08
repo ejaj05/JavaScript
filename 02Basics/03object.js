2962777c12fff79fdaf3f95f7029efb70cfa65cd
ejaj05 <alamejaj288@gmail.com>
2024-09-08 13:17:02 +0000

    Object

---
+++ 02Basics/03object.js
@@ -0,0 +1,35 @@
+const mySym = Symbol("myKey1")
+
+const JsUser = {
+    name:"Ejaj",
+    "Full name" : "Ejaj Alam",
+    age:22,
+    [mySym]:"myKey1",
+    email:"ejaj@google.com",
+    location:"kolkata",
+    isLogedIn:false,
+    lastLoginDays:["Monday", "Saturday"],
+    greeting : function(){
+        console.log("Hello js User")
+    },
+    greetingTwo : function(){
+        console.log(`Hello JS User ${this.name}`)
+    }
+}
+
+// console.log(JsUser.name)
+// console.log(JsUser["Full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "ejaj@microsoft.com"
+// console.log(JsUser.email)
+// Object.freeze(JsUser) // can't change
+// JsUser.email = "ejaj@amazon.com"
+// console.log(JsUser.email)
+
+//fucntion in an object
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
+
+
+
