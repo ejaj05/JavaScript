1a6003e0f334c57f05ef44a9065114a27e845ffb
ejaj05 <alamejaj288@gmail.com>
2024-09-10 04:33:45 +0000

    control Flow

---
+++ 04controlFlow/01switch.js
@@ -0,0 +1,22 @@
+const month = 3
+
+switch(month){
+    case 1 :
+        console.log("January")
+        break;
+    case 2 :
+        console.log("Febrauary")
+        break;
+    case 3 :
+        console.log("March")
+        break;
+    case 2 :
+        console.log("April")
+        break;
+    case 2 :
+        console.log("May")
+        break;
+    default:
+        console.log("Not valid input")
+        break;
+}
---
+++ 04controlFlow/02truthy.js
@@ -0,0 +1,5 @@
+// falsy value
+// false, 0, -0, BigInt 0n, null, NaN, Undefined, ""
+
+//truthy value
+// "0", 'falsy', [], {}, function(){}
