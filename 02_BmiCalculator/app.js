6ec143742527d80df5e86fa9ee025b8b649f8369
ejaj05 <alamejaj288@gmail.com>
2024-09-19 04:04:08 +0000

    Bmi Calculator

--- 02_BmiCalculator/app.js
+++ 02_BmiCalculator/app.js
@@ -1,4 +1,19 @@
-let height = document.querySelector("#height")
-let weight = document.querySelector("#weight")
+const form = document.querySelector("form")
 
-console.log(height);
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let height = parseInt(document.querySelector("#height").value)
+    let weight = parseInt(document.querySelector("#weight").value)
+    let res = document.querySelector(".result")
+    
+    if(height === '' || height < 0 || isNaN(height)){
+        res.innerHTML = `Please enter valid height ${height}`
+    }
+    else if(weight === '' || weight < 0 || isNaN(weight)){
+        res.innerHTML = `Please enter valid weight ${weight}`
+    }
+    else{
+        const bmi = ((weight*10000)/(height*height)).toFixed(2);
+        res.innerHTML = bmi;
+    }
+})
